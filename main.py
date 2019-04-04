a4e2123b7738372f69bb008eddbdb34925ae1ebf
Raphael-Nyphalem <43246557+Raphael-Nyphalem@users.noreply.github.com>
2019-04-04 19:41:54 +0200

    AHHHHH ENFINN

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ lastTime = {}
 
 #asset
 entity_asset = {}
+entity_asset["Player"]={}
 window= None
 
 #entity type
@@ -30,11 +31,18 @@ Mob = None
 Boon = None
 
 Menu = None
-
+#asset Asheiya
+#on le metra dans Init
+Asheiya_asset=[
+"Run_Right_0", "Run_Right_45", "Run_Right_90",  "Run_Right_-45", "Run_Right_-90",
+ "Run_Left_0", "Run_Left_45", "Run_Left_90", "Run_Left_-45", "Run_Left_-90",
+"Wait_Right_0", "Wait_Right_45", "Wait_Right_90", "Wait_Right_-45", "Wait_Right_-90",
+ "Wait_Left_0", "Wait_Left_45", "Wait_Left_90", "Wait_Left_-45", "Wait_Left_-90",
+]
 
 #______INIT________________________________________________________________________
 def Init():
-	global window, TimeStep, lastTime, Menu, Player, entity_asset
+	global window, TimeStep, lastTime, Menu, Player, entity_asset, Ea_p
 	lastTime["dt"] = time.time()
 	lastTime["2s"] = time.time()
 	Menu = "Quiche"
@@ -47,14 +55,18 @@ def Init():
 	tty.setcbreak(sys.stdin.fileno())
 
 	#Player
+	entity_asset["Player"]["position"] = ["Wait","Right","0"]
+	Ea_p = Ea_p=entity_asset["Player"]["position"]
 	X_player = 20
 	Y_player = 37
 	life_player = 18
 	armor_player =25
 	speed_player = 5
 	Player = E.create_entity("Asheiya Briceval", "Player", X_player, Y_player, life_player, armor_player, speed_player)
-	entity_asset["Player"] = E.create_asset("Player.txt")
-
+	for Asheiya_doc in ["Run_Right_0","Wait_Right_0","Run_Left_0","Wait_Left_0"]:# a terme on utilisera "Asheiya_asset" ou un constructeur de txt
+		entity_asset["Player"][Asheiya_doc]=E.create_asset("Asheiya/Asset/" + Asheiya_doc + ".txt") #chargement Asset
+	entity_asset["Player"]["FramesNb"]=0
+	print entity_asset
 	#effacer la console
 	sys.stdout.write("\033[1;1H")
 	sys.stdout.write("\033[2J")
@@ -62,13 +74,14 @@ def Init():
 
 #______SHOW________________________________________________________________________
 def show():
-	global window, TimeStep, lastTime, Menu,Player, entity_asset
+	global window, TimeStep, lastTime, Menu,Player, entity_asset, Ea_p
 	#
 	#Show Frame
 	if time.time() >= lastTime["dt"] + TimeStep:
 		B.show_wd(window)
 		#if Menu == "Quiche" :
-		E.show_entity(entity_asset["Player"],Player, 40, 33)
+		print Ea_p[0]+"_"+Ea_p[1]+"_"+Ea_p[2]
+		E.show_entity(entity_asset["Player"][str(Ea_p[0]+"_"+Ea_p[1]+"_"+Ea_p[2])], Player, 40, 33)
 		lastTime["dt"] = time.time()
 	#end loop
 	if time.time() >= lastTime["2s"] + 2:
@@ -87,11 +100,18 @@ def Interact():
 	def isData():
 		#recuperation evenement clavier
 		return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
-	global Menu, Player
+
+	global Menu, Player, Ea_p
 	if isData() :
 		c = sys.stdin.read(1)
 		if c == '\x1b': # \x1b = esp
 			quitGame()
+		elif c == "l":
+			Ea_p=[Ea_p[0],"Right",Ea_p[2]]
+		elif c == "j":
+			Ea_p=[Ea_p[0],"Left",Ea_p[2]]
+	else:
+		Ea_p = ["Wait",Ea_p[1],Ea_p[2]]
 
 #_____MOVE_________________________________________________________________________
 def move():

c08e99656ef8d7dff8b930f72a98f141085384b2
Raphael-Nyphalem <43246557+Raphael-Nyphalem@users.noreply.github.com>
2019-06-06 16:44:58 +0200

    gril

--- grid.py
+++ grid.py
@@ -1,12 +1,50 @@
 #-*- coding:utf-8 -*
-
 void_collision ="0"
-random_zone="O"
-damage_Zone= "¤"
 _wall = "X"
 Gostwall = "-"
-take_damage = "."
+EntityHitbox = "E"
 
+import files
+
+def Create_Grid(CG_filename):
+	"""
+    DESCRIPTION
+    ===========
+        créé une grille du jeux
+
+    PARAM
+    =====
+
+    @param CG_filenamet: fichier a faire la grille
+    @type CG_filename : str
+
+    RETOUR
+    ======
+
+    @return SEG_grid: grille du jeu avec l'entité en moins
+    @rtype SEG_grid: list
+	"""
+	ca=dict()
+	myfile= files.OPEN_FILE_XML(CG_filename)
+	mylist = myfile.split("\r\n")
+	myGrid=[]
+	for y in range(len(mylist)):
+		myGrid.append([])
+		for x in range(len(mylist[y])):
+			myGrid[y].append([])
+			myGrid[y][x]={}
+			element = mylist[y][x]
+			if element == " ":
+				myGrid[y][x]["Backgound"]=void_collision
+			elif element == "=":
+				myGrid[y][x]["Backgound"]=Gostwall
+			elif element in ["X","-","+"]:
+				myGrid[y][x]["Backgound"]=_wall
+			myGrid[y][x]["Entity"]={}
+
+	files.SAVE_FILE_JSON(myGrid,"log_wtf")
+	return
+Create_Grid("GameZone/Windows.txt")
 
 def Supr_Ent_Grid(SEG_ent, SEG_grid) :
 	"""

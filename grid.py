e45fd15ae8e00fc969ebeae350760b7d341b17d0
Mannt <47539780+xXManntXx@users.noreply.github.com>
2019-06-06 16:20:20 +0200

    add ent to grid done

--- grid.py
+++ grid.py
@@ -32,6 +32,44 @@ def Supr_Ent_Grid(SEG_ent, SEG_grid) :
 	for SEG_y in SEG_ent["Asset"][ActualAsset]["Shadow"] :
 		for SEG_x in SEG_y :
 			if SEG_x != void_collision :
-				SEG_grid[SEG_ent["y"]+SEG_y][SEG_ent["x"]+SEG_x].remove(SEG_ent)
+				SEG_grid[SEG_ent["y"]+SEG_y][SEG_ent["x"]+SEG_x]["Entity"].remove(SEG_ent)
 
 	return SEG_grid
+
+
+def Add_Ent_Grid(SEG_ent, SEG_grid, SEG_collision=False) :
+	"""
+    DESCRIPTION
+    ===========
+        Supprime une entité de la grille de jeu
+
+    PARAM
+    =====
+
+    @param SEG_ent: entité à ajouter à la grille
+    @type SEG_ent : dict
+
+    @param SEG_grid: grille du jeu
+    @type SEG_grid: str
+
+    @param SEG_collision: liste des entités présents sur les cases ou l'entité a été ajouté
+    @type SEG_collision: list
+
+    RETOUR
+    ======
+
+    @return SEG_grid: tuple composé de la grille avec l'entité en plus et si demandé, les entités déjà présentent sur les cases où a été ajouté l'entité
+    @rtype SEG_grid: tuple
+"""
+	SEG_collided_ent=[]
+
+	for SEG_y in SEG_ent["Asset"][ActualAsset]["Shadow"] :
+		for SEG_x in SEG_y :
+			if SEG_x != void_collision :
+				if SEG_collision :
+					for SEG_what_ent in SEG_grid[SEG_ent["y"]+SEG_y][SEG_ent["x"]+SEG_x]["Entity"] :
+						SEG_collided_ent.append(SEG_what_ent)
+				SEG_grid[SEG_ent["y"]+SEG_y][SEG_ent["x"]+SEG_x]["Entity"].append(SEG_ent)
+
+	return SEG_grid
+
